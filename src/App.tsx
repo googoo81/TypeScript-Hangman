09c024a03bdf771ae6946d45f166ee27f594bf4e
송현우 <shw9052@naver.com>
2023-05-25 21:08:20 +0900

    hangman drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import { useState } from 'react'
+import { useState } from "react";
+import words from "./WordList.json";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("Test")
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   return (
-    <h1>Hi</h1>
-  )
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "2rem",
+          textAlign: "center",
+        }}
+      >
+        Lose Win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
-export default App
+export default App;
